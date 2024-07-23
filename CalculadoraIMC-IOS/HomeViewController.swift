707fb9d36993b9d7f1d56980928023888e94cf33
raulfl1982ndrd <raulfl1982ndrd@gmail.com>
2024-07-23 10:07:34 +0200

    Initial Commit

---
+++ CalculadoraIMC-IOS/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculadoraIMC-IOS
+//
+//  Created by Mañanas on 22/7/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+    
+}
+
